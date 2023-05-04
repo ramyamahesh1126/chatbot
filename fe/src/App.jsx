b2526be9c04ce34a71ff0e3f01e9bf7c8c20571e
Ashika Anand Babu <Ashika.makam@gmail.com>
2023-05-04 14:20:02 -0700

    Front end commit

---
+++ fe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Chatbot from './components/Chatbot';
+import MainPage from './components/MainPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+      {/* <Chatbot /> */}
+    </div>
+  )
+}
+
+export default App
